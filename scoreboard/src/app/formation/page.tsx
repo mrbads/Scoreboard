671bbc04d479d848a8cc0b1517994fb33ed07098
Bas van der Borden <bascasper@gmail.com>
2024-11-23 22:58:00 +0100

    formationprops removed

--- scoreboard/src/app/formation/page.tsx
+++ scoreboard/src/app/formation/page.tsx
@@ -2,10 +2,6 @@
 
 import { useEffect, useMemo, useState } from "react";
 
-interface FormationProps {
-    players: string[];
-}
-
 export default function Formation() {
     const players = useMemo(() => ["Player 1", "Player 2", "Player 3", "Player 4", "Player 5", "Player 6", "Player 7", "Player 8", "Player 9", "Player 10", "Player 11"], []);
 
