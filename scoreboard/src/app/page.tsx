e7437312ff78a11605c57a659cb4fc60cf0891f5
Bas van der Borden <bascasper@gmail.com>
2024-11-23 22:32:41 +0100

    removed unused vars

--- scoreboard/src/app/page.tsx
+++ scoreboard/src/app/page.tsx
@@ -8,13 +8,10 @@ import { Button, Card, CardBody, CardHeader, Input } from "@nextui-org/react";
 import TeamScore from "./components/TeamScore";
 
 export default function Home() {
-  const team1 = "Team 1";
-  const team2 = "Team 2";
   const [score1, setScore1] = useState(0);
   const [score2, setScore2] = useState(0);
   const [resetTimer, setResetTimer] = useState(false);
   const players = ["Player 1", "Player 2", "Player 3", "Player 4", "Player 5", "Player 6", "Player 7", "Player 8", "Player 9", "Player 10", "Player 11"];
-  const [opstelling, setOpstelling] = useState(new Set([]));
   const [startingMinutes, setStartingMinutes] = useState(0);
   const [halfLength, setHalfLength] = useState(2);
   const [isRunning, setIsRunning] = useState(false);
